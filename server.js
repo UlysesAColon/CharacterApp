98f03b2fe3a4394bd43e740bfdea2f12296ee6ce
Ulyses <ulysesacolon@gmail.com>
2019-05-14 17:36:01 -0400

    working off the old app for ideas

--- server.js
+++ server.js
@@ -3,14 +3,28 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
+
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname,'client', 'build', 'index.html'));
-});
-app.listen(port);
+
+// app.use(express.static(path.join(__dirname, 'client/public')));
+
+//if (process.env.NODE_ENV === 'production') {
+//  app.use(express.static('client/public/'));
+//  app.get('*', function(req, res){
+//    res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+//  });
+//}
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname,'client','build','index.html'));
+  });
+  
+   //build
+   app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname+'/client/public/index.html'));
+   })
+  
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Server started on port ${port}`));
