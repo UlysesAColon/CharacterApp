a60d6000efd01425e99e491d5b51ac30d044ea81
Ulyses <ulysesacolon@gmail.com>
2019-05-14 17:41:40 -0400

    double port issue

--- server.js
+++ server.js
@@ -25,6 +25,5 @@ app.get('/*', (req,res) =>{
    app.get('/*', (req, res) => {
      res.sendFile(path.join(__dirname+'/client/public/index.html'));
    })
-  
-  const port = process.env.PORT || 5000;
+
   app.listen(port, () => console.log(`Server started on port ${port}`));
